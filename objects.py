be1c875ae43537320faa5d6c268e1d2378da014a
felix-824 <nf94new@gmail.com>
2026-05-11 19:23:57 +0900

    feat: specific error handling system

--- objects.py
+++ objects.py
@@ -23,3 +23,18 @@ print("result1:", result1)
 
 result2 = ceil(98.7)
 print("result2:", result2)   
+
+
+print("==== Error handling system ====")
+car_dict = dict(name="Tayota", year=2026, electric=True)
+
+try:
+    print("passed here")
+    result = car_dict["origin"]
+    print("result:", result)
+except KeyError as err:
+    print("No origin state property found:", err)
+else:
+    print("Executed  successfully without errors")
+finally:
+    print("Final closing logic")
